075b97288f3dacd431577b13313057f9b0200907
LedriNimani <ledrinimani2018@gmail.com>
2025-05-13 17:46:04 +0200

    ....

---
+++ Module 4/ForLoops.py
@@ -0,0 +1,24 @@
+names=["Alma","Blerta","Dhurata","Shpati"]
+for name in names:
+    print(name)
+
+
+
+    sentence_shembulli="Hello, World5"
+    for chr in sentence_shembulli:
+        if char.isalpha(): #kthen true or false if char is a letter
+            print(char)
+
+            #shembulli 3 me range function
+            for numri in range(1,6): #range(x,y) x-ku fillon  y-ku mbaron  mirepo nuk e perfshin y
+                print(numri)
+
+                #gjeje max ne nje list
+
+             numrat=[33,45,45,55,23,12,94,100,101]
+              max=numrat[0]
+                for num in numrat:
+                    if num>max:
+                        max=num
+                        print("The max value is",max)
+
---
+++ Module 4/WhileLoop.py
@@ -0,0 +1,14 @@
+count=1
+while count<=5:
+
+   print("Iteration",count)
+   count+=1
+
+   #shembull gjene shumen e numrave prej 1 deri ne 100 me loop
+   i=0
+   shume=0
+
+   while i<=100:
+       shume=shume+i
+       i=i+1
+       print("Shuma e numrave eshte" ,shume)
