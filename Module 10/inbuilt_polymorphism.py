24dbc9858a82e18d67145f97c7e1339aabf1f686
ledri11111 <ledrinimani2018@gmail.com>
2025-06-03 17:44:35 +0200

    ....

---
+++ Module 10/inbuilt_polymorphism.py
@@ -0,0 +1,18 @@
+string_length=len("Hello Erin")
+list_length=len([1,27,378,5,14])
+tuple_length=len((1,18,23))
+
+
+print(string_length)
+print(list_length)
+print(tuple_length)
+
+sum_of_string=sum([1,2,3,4,5])
+sum_of_tuple=sum((1,17,2))
+
+print(sum_of_string)
+print(sum_of_tuple)
+
+
+numbers = max([10, 25, 7, 42, 3])
+print(numbers)
---
+++ Module 10/poly_functions.py
@@ -0,0 +1,13 @@
+def add(x,y):
+    return (x+y)
+def concatenate(x,y):
+    return str(x) + str(y)
+def operator(operation, x, y):
+    return operation(x,y)
+
+result_sum=operator(add, 5, 2)
+result_con=operator(concatenate, "Hello", "Ylli")
+
+print(result_sum)
+print(result_con)
+
