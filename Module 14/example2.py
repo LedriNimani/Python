3e1d59f5d5964155e76f3a78ba7a53e49cad6e40
ledri11111 <ledrinimani2018@gmail.com>
2025-06-24 17:46:04 +0200

    ....

---
+++ Module 14/example2.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#getting data from file
+df=pd.read_csv("avgIQpercountry.csv")
+
+avq_iq=df.groupby("Continent")["Average IQ"].mean()
+
+plt.figure(figsize=(10,6))
+
+avg_iq.plot(kind="line",marker="o",color="skyblue")
+
+plt.title("Average IQ by Continent")
+plt.xlabel("Continent")
+plt.ylabel("Average IQ")
+plt.show()
+
--- PythonProject2/KaggleExample.py
+++ PythonProject2/KaggleExample.py
@@ -17,3 +17,21 @@ print(null_count)
 
 #removing duplicates
 df.drop_duplicates(keep="first",inplace=True)
+
+#finding avg of a acontinent
+avg_iq_per_country=df.groupby('Continent'["Average IQ"]).mean()
+
+
+avg_iq_per_country=avg_iq_per_country.sort_values(ascending=False)
+print(avg_iq_per_country)
+
+#calculate nobel prizes by country and show countries only with more than 1 nobel
+#you have to use Group By, Sum and Sort Values
+
+total_nobel_by_country=df.groupby("Country"["Nobel prices"].sum()
+sorted_total_nobel_by_country=total_nobel_by_country.sort_values(ascending=False)
+sorted_total_nobel_by_country=sorted_total_nobel_by_country[sort_total_nobel_by_country!=0]
+print(sorted_total_nobel_by_country)
+
+
+
